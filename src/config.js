44977ba96a26609a718802ebe7ae8c8f4cf76fe8
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-12-17 14:22:17 +0200

    default ports +1

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const epicConfigFile = readEnvironmentVariable('EPIC_CONFIG_FILE');
 export const maxJobsInProcess = readEnvironmentVariable('MAX_JOBS_IN_PROCESS', {defaultValue: 4, format: v => Number(v)});
 
 // Mongo variables to job
-export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27017/db'});
+export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27018/db'});
 
 // OAI-PMH connection variables
 export const oaiPmhAutNamesUrl = readEnvironmentVariable('OAI_PMH_AUT_NAMES_URL', {defaultValue: 'OAI_PMH_AUT_NAMES_URL MISSING'});
